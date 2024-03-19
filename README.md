589060fae116c51fca9f066729dc48cc8dc2cbeb
Gmin77 <gongminseung@gmail.com>
2024-03-19 12:19:40 +0900

    add

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# OpenWeather API를 사용한 실시간 온도 측정 및 5일 기온 예측분석
+# OpenWeather API를 사용한 실시간 온도 측정
+# 5일 기온 예측분석
 
+- 원하는 지역을 선택하여 현재기온, 최고/저 온도확인, 날씨정보 등 다양한 정보제공을 위해 제작
+  
+- SideBar (프로젝트에 관한 설명, 지역별 날씨 및 온도 정보, 지난 검색 데이터)
+![SideBar](C:\Users\KK\workspace\1Project\img\1.png "")
--- page2.py
+++ page2.py
@@ -81,93 +81,93 @@ def weather():
     with col8 :
         st.write(f'humidity : {data['main']['humidity']} %')
 
-        st.divider()
-        st.header('5일 주기 기온차트')
+    st.divider()
+    st.header('5일 주기 기온차트')
 
-        # 5일치 기상예측 API추가
-        apis = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric'
-        forecast_result = requests.get(apis)
-        datas = json.loads(forecast_result.text)
+    # 5일치 기상예측 API추가
+    apis = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric'
+    forecast_result = requests.get(apis)
+    datas = json.loads(forecast_result.text)
 
-        print(datas)
+    print(datas)
 
-        data_to_write = []
-        date_check ={} # 값을 추가해줄 딕셔너리
-        max_temp = {}
-        min_temp = {}
+    data_to_write = []
+    date_check ={} # 값을 추가해줄 딕셔너리
+    max_temp = {}
+    min_temp = {}
 
-        if "list" in datas :
-            for forecast in datas['list'] :
-                data_string = forecast['dt_txt']
-                date_object = datetime.datetime.strptime(data_string, '%Y-%m-%d %H:%M:%S')
-                day = date_object.strftime('%d')  # day만 출력 
-                # print(f'Date: {date_object.date()}, Day: {day}')
+    if "list" in datas :
+        for forecast in datas['list'] :
+            data_string = forecast['dt_txt']
+            date_object = datetime.datetime.strptime(data_string, '%Y-%m-%d %H:%M:%S')
+            day = date_object.strftime('%d')  # day만 출력 
+            # print(f'Date: {date_object.date()}, Day: {day}')
 
-                # if date_object == day :
-                #     date_check[day] = forecast['dt_txt'].index
-                
-                #해당 날짜를 구분하여 index의 길이를 측정하여 갯수파악
-                if day not in date_check:
-                    date_check[day] = [datas['list'].index(forecast)]
-                else:
-                    date_check[day].append(datas['list'].index(forecast))
+            # if date_object == day :
+            #     date_check[day] = forecast['dt_txt'].index
+            
+            #해당 날짜를 구분하여 index의 길이를 측정하여 갯수파악
+            if day not in date_check:
+                date_check[day] = [datas['list'].index(forecast)]
+            else:
+                date_check[day].append(datas['list'].index(forecast))
 
-            for day, index in date_check.items(): # 딕셔너리 day, index의 값을 출력
-                print(f"Date: {day}, Index: {index}")
+        for day, index in date_check.items(): # 딕셔너리 day, index의 값을 출력
+            print(f"Date: {day}, Index: {index}")
 
-            date_check1 = list(date_check.items())
-            length = len(date_check1)
-            print(date_check1)
+        date_check1 = list(date_check.items())
+        length = len(date_check1)
+        print(date_check1)
 
-            for i in range(length):
-                key, value = date_check1[i]
-                # print(f"{key}, {value}")
+        for i in range(length):
+            key, value = date_check1[i]
+            # print(f"{key}, {value}")
 
-            for key in date_check.keys():
-                values = date_check[key]
-                for idx, value in enumerate(values, start=1):
-                    print(f"{int(key)}day: {value}")
-                    check_list = datas['list'][value]
-                    check_list1 = check_list['main']['temp_min'] #최저온도
-                    check_list2 = check_list['main']['temp_max'] #최대온도
-                    # print(check_list)
-                    # print('최저 온도 :',check_list1,'°C')
-                    # print('최대 온도 :',check_list2,'°C')
-                    
-                        # 최고온도와 최저온도를 딕셔너리에 저장
-                    if key not in max_temp:
-                        max_temp[key] = check_list2
-                    elif check_list2 > max_temp[key]:
-                        max_temp[key] = check_list2
-
-                    if key not in min_temp:
-                        min_temp[key] = check_list1
-                    elif check_list1 < min_temp[key]:
-                        min_temp[key] = check_list1
-
-                    print(max_temp)
-                    print(min_temp)
-
-            # for key, values in date_check.items():
-                # for value in values:
-                city = selected_city_index
+        for key in date_check.keys():
+            values = date_check[key]
+            for idx, value in enumerate(values, start=1):
+                print(f"{int(key)}day: {value}")
                 check_list = datas['list'][value]
-                day = check_list['dt_txt'].split()[0]  # 날짜
-                temp_max = max_temp[key]  # 최고 온도
-                temp_min = min_temp[key]  # 최저 온도
-                data_to_write.append([city, temp_min, temp_max, day])
+                check_list1 = check_list['main']['temp_min'] #최저온도
+                check_list2 = check_list['main']['temp_max'] #최대온도
+                # print(check_list)
+                # print('최저 온도 :',check_list1,'°C')
+                # print('최대 온도 :',check_list2,'°C')
+                
+                    # 최고온도와 최저온도를 딕셔너리에 저장
+                if key not in max_temp:
+                    max_temp[key] = check_list2
+                elif check_list2 > max_temp[key]:
+                    max_temp[key] = check_list2
 
-            # CSV 파일에 데이터 쓰기
-            filename = 'record_data.csv'
-            write_csv(data_to_write, filename)
+                if key not in min_temp:
+                    min_temp[key] = check_list1
+                elif check_list1 < min_temp[key]:
+                    min_temp[key] = check_list1
 
-            print("최고 온도:")
-            for key, value in max_temp.items():
+                print(max_temp)
+                print(min_temp)
+
+        # for key, values in date_check.items():
+            # for value in values:
+            city = selected_city_index
+            check_list = datas['list'][value]
+            day = check_list['dt_txt'].split()[0]  # 날짜
+            temp_max = max_temp[key]  # 최고 온도
+            temp_min = min_temp[key]  # 최저 온도
+            data_to_write.append([city, temp_min, temp_max, day])
+
+        # CSV 파일에 데이터 쓰기
+        filename = 'record_data.csv'
+        write_csv(data_to_write, filename)
+
+        print("최고 온도:")
+        for key, value in max_temp.items():
+            print(f"{key}: {value}°C")
+
+        print("최저 온도:")
+        for key, value in min_temp.items():
                 print(f"{key}: {value}°C")
-
-            print("최저 온도:")
-            for key, value in min_temp.items():
-                    print(f"{key}: {value}°C")
 
     df_max = pd.DataFrame(max_temp.items(), columns=['Date', 'Max Temp'])
     df_min = pd.DataFrame(min_temp.items(), columns=['Date', 'Min Temp'])
@@ -180,7 +180,7 @@ def weather():
     plt.plot(df_min['Date'], df_min['Min Temp'], label='Min Temperature', marker='*')
     plt.xlabel('Date')
     plt.ylabel('Temperature (°C)')
-    plt.title('Weekly Temperature Forecast')
+    plt.title(f'Weekly Temperature Forecast {city}')
     plt.xticks(rotation=45)
     plt.legend()
     plt.tight_layout()
